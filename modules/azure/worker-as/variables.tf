2cbb8416509df6cb0674bc8600a5f501babe4b95
Johannes 'fish' Ziemke <github@freigeist.org>
2017-07-12 16:46:04 +0200

    maybe as map?

--- modules/azure/worker-as/variables.tf
+++ modules/azure/worker-as/variables.tf
@@ -83,6 +83,6 @@ variable "kubelet_node_label" {
 
 variable "public_ip_address_ids" {
   description = "Optional public IP ids to associate with workers"
-  type = "list"
-  default = []
+  type = "map"
+  default = {}
 }
